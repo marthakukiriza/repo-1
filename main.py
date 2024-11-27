780b7b6cbc7dfa470bb698e2be29bd7cd5abdcdc
marthakukiriza <kukirizamartha@gmail.com>
2024-11-27 14:16:36 +0300

    added auth

--- main.py
+++ main.py
@@ -7,3 +7,20 @@ def outer(x):
     inner()
 
 outer(2)
+
+
+class Person:
+    def __init__(self):
+        pass
+
+    def print_hello():
+        print("Hello, I'm new to git")
+
+    def auth(password):
+        passkey = "hello230"
+        does_match = password == passkey
+        if does_match:
+            print("Password Matches")
+        else:
+            print("Wrong password")
+        return does_match            
